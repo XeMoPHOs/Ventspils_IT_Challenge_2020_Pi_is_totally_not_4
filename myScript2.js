4f4656d1284b87329cdf7e74f3d467eb03fe8134
Jiggernaut152 <72450506+Jiggernaut152@users.noreply.github.com>
2020-10-06 18:58:22 +0300

    Update myScript2.js

--- myScript2.js
+++ myScript2.js
@@ -2,19 +2,24 @@ let gui;
 let s_CO2;
 let s_time;
 let temp;
-const CO2_2019 = 410;
+let CO2;
+const CO2_2020 = 410;
+const CO2_per_month = 0.18;
 
 function setup(){
     createCanvas(1600, 800);
 
     gui = createGui();
+
     s_CO2 = createSlider(0, 400);
     s_CO2.position(10,10);
     s_CO2.style('width', '160px');
     s_time = createSlider(0, 240);
     s_time.position(10,30);
     s_time.style('width', '160px');
-    s_CO2.value() = 200;}
+    s_CO2.value() = 200;
+}
+
 function draw() {
     background(220);
     fill(255, 0, 0);
@@ -22,7 +27,7 @@ function draw() {
 }
 
 function temperature() {
-
+    temp = s_time.value()*CO2_per_month;
 }
 
 
