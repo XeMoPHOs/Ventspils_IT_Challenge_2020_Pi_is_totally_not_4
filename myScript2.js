ed0e3d9b733abeecf18c1379ce5afebedfbcc0bd
XeMoPHOs <52276695+XeMoPHOs@users.noreply.github.com>
2020-10-06 18:28:51 +0300

    15

--- myScript2.js
+++ myScript2.js
@@ -1,25 +1,29 @@
 let gui;
-let slider;
+let s_CO2;
+let s_time;
 let temp;
+const CO2_2019 = 410;
 
 function setup(){
     createCanvas(1600, 800);
 
     gui = createGui();
-    slider = createSlider(0, 400);
-    slider.position(10,10);
-    slider.style('width', '160px');
-    val = s
+    s_CO2 = createSlider(0, 400);
+    s_CO2.position(10,10);
+    s_CO2.style('width', '160px');
+    s_time = createSlider(0, 240);
+    s_time.position(10,30);
+    s_time.style('width', '160px');
+    s_CO2.value() = 200;
 }
 
 function draw() {
     background(220);
     fill(255, 0, 0);
-    ellipse(slider.value(), 300, 100);
+    ellipse(s_CO2.value(), 300, 100);
 }
 
-function touchMoved(){
-    if (slider.value() == 50){
-        return false;
-    }
+function temperature(){
+
 }
+
