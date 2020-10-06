7a0568050fb4b37102efe1427f6ba421bac01041
XeMoPHOs <52276695+XeMoPHOs@users.noreply.github.com>
2020-10-06 18:44:24 +0300

    14

--- myScript2.js
+++ myScript2.js
@@ -1,7 +1,7 @@
 let gui;
 let slider;
 let temp;
-
+    
 function setup(){
     createCanvas(1600, 800);
 
@@ -9,17 +9,10 @@ function setup(){
     slider = createSlider(0, 400);
     slider.position(10,10);
     slider.style('width', '160px');
-    val = s
 }
 
 function draw() {
     background(220);
     fill(255, 0, 0);
     ellipse(slider.value(), 300, 100);
-}
-
-function touchMoved(){
-    if (slider.value() == 50){
-        return false;
-    }
 }
