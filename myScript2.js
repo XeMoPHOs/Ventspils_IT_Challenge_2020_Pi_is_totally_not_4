741221d06b652df0f0b1b8b2696293a251ad74bc
Jiggernaut152 <72450506+Jiggernaut152@users.noreply.github.com>
2020-10-09 18:02:26 +0300

    fix slider pls

--- myScript2.js
+++ myScript2.js
@@ -1,23 +1,23 @@
-let gui;
-let s_CO2;
-let s_time;
-let temp;
+var gui;
+var s_CO2;
+var s_time;
+var temp;
 const CO2_2020 = 410;
 const CO2_per_month = 0.18;
+const temp_2020 = 13.86;
 
 function setup(){
     createCanvas(1600, 800);
-
     gui = createGui();
 
     s_CO2 = createSlider(0, 400);
     s_CO2.position(10,10);
     s_CO2.style('width', '160px');
+    s_CO2.value(200);
+
     s_time = createSlider(0, 240);
     s_time.position(10,30);
     s_time.style('width', '160px');
-    s_CO2.value(200);
-    s_CO2.value(200);
     s_time.value(0);
 }
 function draw() {
@@ -25,17 +25,18 @@ function draw() {
     fill(255, 0, 0);
     ellipse(CO2(), 100, 100);
 }
-function CO2_emissions(){
-    document.getElementById("Slider1").innerHTML = s_CO2.value() / 200;
+document.getElementById("Slider1").innerHTML = 200;
+document.getElementById("Slider2").innerHTML = CO2();
+
+function CO2(){
+    return(s_time.value()*CO2_per_month*s_CO2.value()/200);
 }
-function CO2_amount(){
-    document.getElementById("Slider2").innerHTML = CO2() + CO2_2020;
+function Temp(){
+    return(temp_2020+CO2_per_month*s_time.value()+s_time.value()*(CO2_per_month/CO2_2020)*CO2())
 }
-function CO2() {
-    return(s_time.value()*CO2_per_month*s_CO2.value()/200)
-}
-function myFunction() {
+
+/*function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-
+*/
